1521f5ea640b43c783b245715649f9adb83b16d4
aungkyawminhtet <aungkyawminhtet.sbo@gmail.com>
2026-08-15 15:25:36 +0700

    response data omit and make custom response data with resource

--- app/Http/Controllers/Api/V1/TicketController.php
+++ app/Http/Controllers/Api/V1/TicketController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Requests\Api\V1\UpdateTicketRequest;
+use App\Http\Resources\V1\TicketResource;
 use App\Models\Ticket;
+use Ramsey\Collection\Collection;
 
 class TicketController extends Controller
 {
@@ -14,7 +16,7 @@ class TicketController extends Controller
      */
     public function index()
     {
-        return Ticket::all();
+        return TicketResource::collection(Ticket::all());
     }
 
     /**
@@ -38,7 +40,7 @@ class TicketController extends Controller
      */
     public function show(Ticket $ticket)
     {
-        //
+        return new TicketResource($ticket);
     }
 
     /**
---
+++ app/Http/Resources/V1/TicketResource.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TicketResource extends JsonResource
+{
+
+    public static $wrap = 'Data'; //to change main response data
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'type' => 'ticket',
+            'id' => $this->id,
+            'attributes' => [
+                'title' => $this->title,
+                'description' => $this->description,
+                'status' => $this->status,
+                'createdAt' => $this->created_at->diffForHumans(),
+                'updatedAt' => $this->updated_at->diffForHumans(),
+            ],
+            'relationships' => [
+                'author' => [
+                    'data' => [
+                        'type' => 'user',
+                        'id' => $this->user_id,
+                    ],
+
+                    'links' => [
+                        ['self' => 'todo']
+                    ]
+                ]
+            ],
+            'links' => [
+                'self' => route('tickets.show', ['ticket' => $this->id])
+            ]
+        ];
+    }
+}
